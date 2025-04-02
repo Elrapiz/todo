30d408c4856d3a71a60664707893ab9d746c1b61
Elrapiz <rafael.pizarro.elrapiz@gmail.com>
2025-04-02 21:35:29 +0800

    Update textarea colors and borders

--- src/App.jsx
+++ src/App.jsx
@@ -137,11 +137,11 @@ function App() {
                             <textarea name="task" id="task" placeholder="Add a new task" className="block w-full outline-none"></textarea>
                             <div>
                                 <label htmlFor="dueDate">Due: </label>
-                                <input name="due" id="dueDate" type="date" />
+                                <input name="due" id="dueDate" type="date" className="border rounded-lg px-2 py-1" />
                             </div>
                         </div>
                         <button type="submit" className="p-3 rounded-full font-bold text-4xl hover:bg-gray-200 active:bg-gray-300">
-                            <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5"/>
                             </svg>
                         </button>
